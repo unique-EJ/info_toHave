96fc8ade1fbcebb4d845d7569826f334b784da00
edj <91560320+e1e0@users.noreply.github.com>
2022-01-27 21:35:13 +0000

    Kotlin / html

---
+++ html/src/main/java/com/example/html/html1.kt
@@ -0,0 +1,17 @@
+package com.example.html
+
+import kotlinx.html.*
+import kotlinx.html.dom.*
+import kotlinx.html.stream.appendHTML
+
+fun main() {
+    var qwerty = createHTMLDocument()
+    qwerty.html {
+        body {
+
+        }
+    }
+    System.out.appendHTML().html {
+
+    }
+}
