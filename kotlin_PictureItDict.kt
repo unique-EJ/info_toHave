bdfb950603f6b9486f406b7ad0e835c868968d9f
EDJ <91560320+e1e0@users.noreply.github.com>
2022-05-04 22:44:10 +0100

    "kotlin_PictureItDict.kt" (1.) & build.gradle

---
+++ kotlin_PictureItDict.kt
@@ -0,0 +1,24 @@
+/* 1. do this:
+  *| *| * ...
+ --+--+-- ...
+  *| *| * ...
+ --+--+-- ...
+  *| *| * ...
+ ...
+*/
+
+fun main() {
+    val n = 6
+    // Loop:
+    for (i in 1..(n*2 - 1)) {
+        var x = " * |"
+        if (i % 2 == 0) x = "---+"
+        val y: String = x.substring(0, x.length-1)
+
+        for (j in 1..(n-1)) {
+            print(x)
+        }
+        println(y)
+    }
+}
+// https://kotlinlang.org/docs/command-line.html#create-and-run-an-application
