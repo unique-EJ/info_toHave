12394b8eeca18539345a7d1f4eb9e66ecf565a4e
EDJ <91560320+e1e0@users.noreply.github.com>
2022-06-29 00:25:40 +0100

    com/example/command_line/app.kt

---
+++ command_line/src/main/java/com/example/command_line/app.kt
@@ -0,0 +1,13 @@
+package com.example.command_line
+
+fun main() {
+    println("  _info_ to have, do, make with - app [command line]  ")
+    println("------------------------------------------------------")
+    println(" Home [h]   | App for **saving data/information to    ")
+    println(" ...        | your device** to have, do, make (things)")
+    println("            | with - and to search for connections,   ")
+    println("            | links of things in that data.           ")
+    for (i in 1..4) {
+        println("            |")
+    }
+}
