9e403d6ae213337e4b87ac8c6c05aee7d76222b5
EDJ <91560320+e1e0@users.noreply.github.com>
2022-06-30 14:19:12 +0100

    UI designing...

--- command_line/src/main/java/com/example/command_line/app.kt
+++ command_line/src/main/java/com/example/command_line/app.kt
@@ -1,13 +1,35 @@
 package com.example.command_line
 
-fun main() {
+/*
+#======================================================#
+|  _info_ to have, do, make with - app [command line]  |
+|------------------------------------------------------|
+| Home [h]   | App for **saving data/information to    |
+| Open [o]   | your device** to have, do, make (things)|
+| ...        | with - and to search for connections,   |
+|            | links of things in that data.           |
+|            |                                         |
+|            |                                         |
+|            |                                         |
+|            |                                         |
+|------------------------------------------------------|
+| Search: ___                                          |
+|------------------------------------------------------|
+#======================================================#
+*/
+
+fun ui() {
     println("  _info_ to have, do, make with - app [command line]  ")
     println("------------------------------------------------------")
     println(" Home [h]   | App for **saving data/information to    ")
     println(" ...        | your device** to have, do, make (things)")
-    println("            | with - and to search for connections,   ")
+    println(" ...        | with - and to search for connections,   ")
     println("            | links of things in that data.           ")
     for (i in 1..4) {
         println("            |")
     }
+}
+
+fun main() {
+    ui()
 }
