e9fb72351a66626853598d46a94ef5b77c874201
Edward Jones <11974523-changeable@users.noreply.gitlab.com>
2024-07-20 07:33:27 +0000

    Update supportive-command-line-shell-in-Termux.sh prompt variable

--- supportive-command-line-shell-in-Termux.sh
+++ supportive-command-line-shell-in-Termux.sh
@@ -2,4 +2,6 @@
 # Run in the Termux (terminal emulator) command-line.
 # ----
 
-# ...
+# -> $PREFIX/etc/bash.bashrc
+export PS1='\n[This is a command-line shell interpreter, that displays in a terminal application.]\n['"(current history list number is 0? supportive prompt line"']\n\# \V \h \$ '
+# n...
